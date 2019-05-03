5b7ddea9a9aff41bb5e24e26213f7ce2cff0b402
Marco Tesch <mtesch@tecracer.de>
2019-05-03 11:51:54 +0200

    Added function to get all AWS Access Key Ages

--- src/aws-keyrotation.py
+++ src/aws-keyrotation.py
@@ -18,6 +18,32 @@ def __getAwsIamUserList(iamClient):
     return users
 
 
+def __getAwsAccessKeyAge(iamClient, iamUsers):
+    '''read the AWS IAM Access Key Age, for each provided user'''
+    iamAccessKeys = {'Keys': []}
+
+    for iamUser in iamUsers:
+        response = iamClient.list_access_keys(UserName=iamUser['UserName'])
+
+        accessKeyInfos = []
+
+        for accessKey in response['AccessKeyMetadata']:
+            accessKeyInfos.append({
+                'AccessKeyId': accessKey['AccessKeyId'],
+                'AccessKeyStatus': accessKey['Status'],
+                'CreateDate': accessKey['CreateDate']
+            })
+
+        if len(accessKeyInfos) > 0:
+            iamAccessKeys['Keys'].append({
+                'UserName': iamUser['UserName'],
+                'AccessKeyInfos': accessKeyInfos
+            })
+
+    return iamAccessKeys
+
+
 if __name__ == "__main__":
     iamClient = boto3.client('iam')
-    __getAwsIamUserList(iamClient)
+    iamUsers = __getAwsIamUserList(iamClient)
+    iamAccessKeys = __getAwsAccessKeyAge(iamClient, iamUsers)
